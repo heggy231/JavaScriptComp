c98abc8276f0dcae08e3ae0b11076224d7da6587
Heggyhere <heggyy@gmail.com>
2017-07-31 19:57:26 -0700

    class es6 note

--- LectureNote/Note/JSCompLectureNotes/InheritanceESClass.js
+++ LectureNote/Note/JSCompLectureNotes/InheritanceESClass.js
@@ -1,38 +1,3 @@
-Date: 7/31
-
-How many ways to create Obj:
-1. Constructor function  >> Inheritance requ/ mult. obj needed with sam blueprint
-2. obj literal >> Simple Obj definition created on the fly
-3. Factory Pattern >> Messaging needs to be done before creating the obj.
-4. Obj constructor >> Simple Obj def. created on the fly
-  - var obj = new Object();
-5. Obj create >> Usu. for the inheritance purposes
-
----
-Obj create demo - directly points to the object
-obj constructor - instances points to the prototype
-(refer to his slides he will share at the end of the class using python tutor)
-
-let personProto = {
-    calculateAge: function () {
-        return 2017 - this.dob;
-    }
-}
-
-let john = Object.create(personProto);
-
-john.name = "John";
-john.dob = 1985;
-john.job = "Software Arch";
-
-console.log(john.calculateAge());
-
-
-Private int sum (int a, int b) {
-  int result = 
-}
-
-- class 
 
 class Person {
     constructor() {
